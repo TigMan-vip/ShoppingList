bccd8fa31e805c01b1a08d4918632a14f6466402
TigMan-vip <tigran200014@mail.ru>
2023-01-16 15:35:19 +0700

    Создал интерфейс репозитория и связал с юзкейсами

--- app/src/main/java/com/example/shoppinglist/Domain/AddShopItemUSeCase.kt
+++ app/src/main/java/com/example/shoppinglist/Domain/AddShopItemUSeCase.kt
@@ -1,8 +1,8 @@
 package com.example.shoppinglist.Domain
 
-class AddShopItemUSeCase {
+class AddShopItemUSeCase(private val shopListRepository: ShopListRepository) {
 
 	fun addShopItem(shopItem: ShopItem) {
-		TODO()
+		shopListRepository.addShopItem(shopItem)
 	}
 }
--- app/src/main/java/com/example/shoppinglist/Domain/GetShopListUseCase.kt
+++ app/src/main/java/com/example/shoppinglist/Domain/GetShopListUseCase.kt
@@ -1,8 +1,8 @@
 package com.example.shoppinglist.Domain
 
-class GetShopListUseCase {
+class GetShopListUseCase(private val shopListRepository: ShopListRepository) {
 
 	fun getShopList(): List<ShopItem> {
-		TODO()
+		return shopListRepository.getShopList()
 	}
 }
