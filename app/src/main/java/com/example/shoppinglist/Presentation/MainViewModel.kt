0f6849aac78d8db281b0b0d0385b4d5ab0896baf
TigMan-vip <tigran200014@mail.ru>
2023-01-17 13:51:35 +0700

    Добавил ViewModel, пока без принчипов чистой архитектуры

---
+++ app/src/main/java/com/example/shoppinglist/Presentation/MainViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.shoppinglist.Presentation
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.shoppinglist.Data.ShopListRepositoryImpl
+import com.example.shoppinglist.Domain.DeleteShopItemUseCase
+import com.example.shoppinglist.Domain.EditShopItemUseCase
+import com.example.shoppinglist.Domain.GetShopListUseCase
+import com.example.shoppinglist.Domain.ShopItem
+import com.example.shoppinglist.Domain.ShopListRepository
+
+class MainViewModel: ViewModel() {
+
+	val repository = ShopListRepositoryImpl
+
+	val getShopListUseCase = GetShopListUseCase(repository)
+	val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+	val editShopItemUseCase = EditShopItemUseCase(repository)
+
+	val shopItemLiveData = MutableLiveData<List<ShopItem>>()
+
+	fun getShopList() {
+		shopItemLiveData.value = getShopListUseCase.getShopList()
+	}
+
+	fun deleteShopItem(shopItem: ShopItem) {
+		deleteShopItemUseCase.deleteShopItem(shopItem)
+		getShopList()
+	}
+
+	fun changeEnableStateShopItem(shopItem: ShopItem) {
+		val newState = shopItem.copy(enabled = !shopItem.enabled)
+		editShopItemUseCase.editShopItem(newState)
+		getShopList()
+	}
+}
